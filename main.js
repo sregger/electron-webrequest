394259d67b785423e423d13f94a63aded6ba0034
Shane Gannon <gannons@cisco.com>
2022-10-23 12:00:12 +0100

    Fixing the issue

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const { app, BrowserWindow, session } = require('electron')
+const { app, BrowserWindow, session, net } = require('electron')
 const path = require('path')
+require("json-circular-stringify");
 
-const filter = null
+// This is the problem. The API takes null but it means no urls are matched
+// const filter = null
+const filter = {
+      urls: ['*://httpbin.org/get']
+    }
+// This intercepts ALL requests
 // const filter = {
-//       urls: ['https://www.github.com/']
+//       urls: []
 //     }
+
+
 
 const createWindow = () => {
     console.log("Creating Window")
@@ -17,19 +25,49 @@ const createWindow = () => {
     })
 
     win.webContents.session.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
-        console.log(`win.webContents.session.webRequest has been called for ${details}`)
-        details.requestHeaders['My-User-Agent'] = 'MyAgent'
+        console.log(`win.webContents.session.webRequest has been called for ${JSON.stringify(details)}`)
+        details.requestHeaders['My-User-Agent-2'] = 'MyAgent'
         callback({ requestHeaders: details.requestHeaders })
     })
 
     win.loadFile('index.html')
+
+    const request = net.request({
+        method: 'GET',
+        protocol: 'http:',
+        hostname: 'httpbin.org',
+        path: '/get',
+        redirect: 'follow'
+    });
+    request.on('response', (response) => {
+        console.log(`STATUS: ${response.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+ 
+        response.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`)
+        });
+    });
+    request.on('finish', () => {
+        console.log('Request is Finished')
+    });
+    request.on('abort', () => {
+        console.log('Request is Aborted')
+    });
+    request.on('error', (error) => {
+        console.log(`ERROR: ${JSON.stringify(error)}`)
+    });
+    request.on('close', (error) => {
+        console.log('Last Transaction has occurred')
+    });
+    request.setHeader('Content-Type', 'application/json');
+    request.end();
 }
 
 app.whenReady().then(() => {
     console.log("Ready!!!!")
     session.defaultSession.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
-        console.log(`session.defaultSession.webRequest has been called for ${details}`)
-        details.requestHeaders['My-User-Agent'] = 'MyAgent'
+        console.log(`session.defaultSession.webRequest has been called for ${JSON.stringify(details)}`)
+        details.requestHeaders['My-User-Agent-1'] = 'MyAgent'
         callback({ requestHeaders: details.requestHeaders })
     })
 
